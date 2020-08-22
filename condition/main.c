1dc977248e9e20ece682fcf4c129e99303b631e0
nissy-shota <47414233+nissy-shota@users.noreply.github.com>
2020-08-22 22:32:58 +0900

    conditional sate if

---
+++ condition/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int year;
+	
+    printf("%p", &year);
+	printf("西暦年を入力して下さい:");
+    
+	scanf("%d",&year);
+	
+	if (year % 4 == 0) printf("夏季五輪\n");
+	if (year % 2 == 0 && year % 4 != 0) printf("冬季五輪\n");
+	if (year % 2 != 0) printf("五輪なし\n");
+	
+	return 0;
+}
