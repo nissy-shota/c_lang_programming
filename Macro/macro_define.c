797aa5f750b8f98f47d97c0663b1a0e96cb2253e
nissy-shota <47414233+nissy-shota@users.noreply.github.com>
2020-08-29 22:08:16 +0900

    macro define

---
+++ Macro/macro_define.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define EXCISETAX 0.03 /* ここで定数を宣言 */
+
+int main(void)
+{
+	int price;
+	printf("本体価格:");
+	scanf("%d",&price);
+	price = (int)((1 + EXCISETAX) * price); /* 定数使用 */
+	printf("税込価格:%d\n",price);
+	return 0;
+}
