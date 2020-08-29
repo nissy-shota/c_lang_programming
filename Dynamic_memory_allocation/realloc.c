8d7ca8df872e2ecab968aeaebf6d26b99022d61c
nissy-shota <47414233+nissy-shota@users.noreply.github.com>
2020-08-30 01:42:31 +0900

    realloc process

---
+++ Dynamic_memory_allocation/realloc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int *heap;
+	heap = (int *)malloc(sizeof(int) * 10);
+	heap = (int *)realloc(heap,sizeof(int) * 100);
+	free(heap);
+	return 0;
+    }
